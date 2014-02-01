a52ddd2aa7fa9106393c3f84829166f0da423c8a
caseymm <caseymm@gmail.cpom>
2014-02-01 17:32:31 -0500

    set up base for index

---
+++ README.md
@@ -0,0 +1,5 @@
+# Docs in Hand
+
+## Second Screen Application
+
+Built with Angular.js
---
+++ app/js/filters.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* Filters */
+
+/*angular.module('docsHandFilters', []).filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+});*/
